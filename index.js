58ffbb9721740340d42116d50fa7e52b90f20ce8
unknown <munyaradzi045@gmail.com>
2024-12-30 12:26:11 +0200

    Add animations

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-function toggleMenu() {
+const toggleMenu = () => {
     const navLinks = document.getElementById('nav-links');
     navLinks.classList.toggle('active');
 }
+
+const menu = document.querySelector('.burger');
+
+menu.addEventListener('click', toggleMenu());
